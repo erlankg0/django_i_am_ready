f27d55d31eb97d240480f686feb67c95bccee5fe
Erlan <era.ab.02@gmail.com>
2021-05-21 18:07:50 +0300

    Update admin panel

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+    fieldsets = [
+        ('Добавления выбора.', {'fields': ['choice_text']}),
+        ('Голос.', {'fields': ['votes']})
+    ]
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Текс для вопроса.", {'fields': ['question_text']}),
+        ('Дата информация.', {'fields': ['pub_date']})
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
 # Register your models here.
